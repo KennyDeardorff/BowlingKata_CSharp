df6059ad46834377f47ad6eb11b991a79e69f9bf
KennyDeardorff <KennyDMailbox-2015@yahoo.com>
2017-04-11 19:44:52 -0600

    - Started limited loop of the rolls for the frames - Added the Approval Tests recieved files to the Git Ignore file

--- BowlingKata/BowlingKata/BowlingGame.cs
+++ BowlingKata/BowlingKata/BowlingGame.cs
@@ -1,18 +1,31 @@
-﻿namespace BowlingKata
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingKata
 {
     public class BowlingGame
     {
+        private List<int> _rolls;
+
         public void Rolls(params int[] rolls)
         {
+            _rolls = rolls.ToList();
         }
 
         public override string ToString()
         {
-            var roll1 = 5;
-            var roll2 = 3;
-            var total = roll1 + roll2;
+            var rollIndex = 0;
+            var output = new List<string>();
 
-            return $"1) {roll1}, {roll2} [{total}] => {total}";
+            for (int frameIndex = 1; frameIndex <= 1; frameIndex++)
+            {
+                var total = _rolls[0] + _rolls[1];
+
+                output.Add($"{frameIndex}) {_rolls[0]}, {_rolls[1]} [{total}] => {total}");
+            }
+
+            return string.Join(Environment.NewLine, output);
         }
     }
 }
