04907f3e57eee3d67917d242e2f5f758806124a6
KennyDeardorff <KennyDMailbox-2015@yahoo.com>
2017-04-11 19:54:18 -0600

    Added a Frame object Parsed the first Frame from the list of rolls

--- BowlingKata/BowlingKata/BowlingGame.cs
+++ BowlingKata/BowlingKata/BowlingGame.cs
@@ -6,26 +6,43 @@ namespace BowlingKata
 {
     public class BowlingGame
     {
-        private List<int> _rolls;
+        private List<Frame> Frames { get; } = new List<Frame>();
 
         public void Rolls(params int[] rolls)
         {
-            _rolls = rolls.ToList();
+            var rollIndex = 0;
+
+            for (int frameIndex = 1; frameIndex <= 1; frameIndex++)
+            {
+                var frame = new Frame();
+                frame.Rolls.Add(rollIndex++);
+                frame.Rolls.Add(rollIndex++);
+            }
         }
 
         public override string ToString()
         {
-            var rollIndex = 0;
             var output = new List<string>();
+            var total = 0;
+            var frameIndex = 0;
 
-            for (int frameIndex = 1; frameIndex <= 1; frameIndex++)
+            foreach (var frame in Frames)
             {
-                var total = _rolls[0] + _rolls[1];
-
-                output.Add($"{frameIndex}) {_rolls[0]}, {_rolls[1]} [{total}] => {total}");
+                total += frame.Rolls.Sum();
+                output.Add($"{frameIndex + 1}) {frame} => {total}");
             }
 
             return string.Join(Environment.NewLine, output);
+        }
+    }
+
+    public class Frame
+    {
+        public List<int> Rolls { get; } = new List<int>();
+
+        public override string ToString()
+        {
+            return $"{string.Join(", ", Rolls)} [{Rolls.Sum()}]";
         }
     }
 }
